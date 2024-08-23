a31ec085e60f5175db1ae0dcd7811082dd612e8e
Ivan Atias <ivan.d.atias@gmail.com>
2024-08-23 02:10:32 -0400

    👀 chore: add JSDOC

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,5 +1,15 @@
 import type { ErrorFormatter } from './types.ts'
 
+/**
+ * Handle the error thrown by the task.
+ *
+ * @param err The error value that was thrown by the task.
+ * @param errorFormatter An optional error formatter that will be called with the thrown error value.
+ * If this argument is provided, it should return the error that will be returned as the second value of the tuple.
+ * The returned value will be of the shape returned by the formatter you passed.
+ * If this argument is not provided, the error will be an `Error` object.
+ * @returns The error that will be returned as the second value of the tuple.
+ */
 export const errorHandler = <ThrowValueType, ReturnedErrorValueType>(
   err: ThrowValueType,
   errorFormatter?: ErrorFormatter<ThrowValueType, ReturnedErrorValueType>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { errorHandler } from './error-handler.ts'
 import type { TryThisResult, ErrorFormatter } from './types.ts'
 
+/**
+ * @description
+ * Perform tasks without having to worry about using try/catch blocks.
+ *
+ * @template TaskType The type of the task to be performed.
+ * @template ThrownValueType The type of the value that might be thrown by the task when it fails. Defaults to `unknown`.
+ * @template ReturnedErrorValueType The type of the value that will be returned as the error in case of failure. Defaults to `Error`.
+ * @param {TaskType} task The task to be performed.
+ * @param {ErrorFormatter<ThrownValueType, ReturnedErrorValueType>} [errorFormatter]
+ * An optional function that will be called with the thrown value if the task fails. It should return the error
+ * that will be returned as the second value of the tuple.
+ * The returned error value will be of the shape returned by the formatter you passed.
+ * @returns {TryThisResult<TaskType, ReturnedErrorValueType>} A tuple where the first value is the result
+ * of the task in case of success, or `null` in case of failure. The second value is an error in case of
+ * failure, or `null` in case of success.
+ */
 export function tryThis<
   TaskType,
   ThrownValueType = unknown,
@@ -30,3 +46,5 @@ export function tryThis<
 
   return task as any
 }
+
+const [result, error] = await tryThis(Promise.resolve('hello'))
